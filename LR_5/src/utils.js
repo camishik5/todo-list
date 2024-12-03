71fb847d871c6b709ecdfd6301262d8adae115b4
camishik5 <163503791+camishik5@users.noreply.github.com>
2024-12-03 11:15:10 +0300

    Delete LR_5 directory

--- LR_5/src/utils.js
+++
@@ -1,9 +0,0 @@
-// utils.js
-
-/**
- * Функция для генерации уникального идентификатора для задачи
- * @returns {string} Уникальный идентификатор задачи
- */
-export function generateUniqueId() {
-    return `task-${Date.now()}-${Math.floor(Math.random() * 10000)}`;
-}
